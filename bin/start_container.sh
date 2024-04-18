64bccc77169489bce71aa59507886667815d420b
kkirscht <korben@galaxy3.net>
2024-04-17 22:30:48 -0700

    Updated files

--- bin/start_container.sh
+++ bin/start_container.sh
@@ -5,13 +5,13 @@ if [ -d ~/lab1/${1} ]
 then
   cd ~/lab1/${1}
 
-  if [ "${0}" == "start_container.sh" || "${0}" == "bin/start_container.sh"]
+  if [ "${0}" == "start_container.sh" || "${0}" == "bin/start_container.sh" ]
   then
     sudo docker-compose up -d
     sudo docker-compose ps
   fi
 
-  if [ ${0} -eq "stop_container.sh" ]
+  if [ "${0}" == "stop_container.sh" ]
   then
     sudo docker-compose ${0}
     sudo docker-compose ps
